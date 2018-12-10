edf9eb5a34cd2c79a90fa6408de73fc54bb85b55
1921622004 <1921622004qwer@gmail.com>
2018-12-10 22:21:00 +0800

    add pagelifeCycle & class properties support

--- src/components/PageLifecycle.js
+++ src/components/PageLifecycle.js
@@ -1,9 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
-class PageLifecycle extends Component{
-  render(){
+class PageLifecycle extends Component {
+  state = { count: 1 }
+
+  updateFn = () => {
+    console.log('============');
+    
+    let { count } = this.state;
+    this.setState({ count: ++count });
+    document.title = `count:${count}`
+  }
+
+  visibilityStateChangeHandler = () => {
+    switch (document.visibilityState) {
+      case 'hidden':
+        clearInterval(this.timer)
+        break;
+      case 'visible':
+        this.timer = setInterval(this.updateFn,1000);
+        break;
+      default:
+        break;
+    }
+  }
+
+  componentDidMount() {
+    this.initialDocTitle = document.title;
+    this.timer = setInterval(this.updateFn, 1000);
+    window.addEventListener('visibilitychange',this.visibilityStateChangeHandler)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+    window.removeEventListener('visibilitychange',this.visibilityStateChangeHandler)
+  }
+
+  render() {
+    const { count } = this.state;
+    console.log(count ,'=== ==== ');
+    
     return (
-      <div>PageLifecycle</div>
+      <Fragment>
+        <h1>PageLifecycle</h1>
+        <p>{count}</p>
+      </Fragment>
     )
   }
 }
