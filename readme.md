f137aee696f4a143a2a70531dfea265ce25a613f
1921622004 <1921622004qwer@gmail.com>
2018-12-15 21:13:58 +0800

    update ambientlight

---
+++ readme.md
@@ -0,0 +1,12 @@
+# 你（可能）不知道的webapi
+
+### 简介
+作为前端er，我们的工作与web是分不开的，随着HTML5的日益壮大，浏览器自带的webapi也随着增多。本篇文章主要选取了几个有趣且有用的webapi进行介绍，包括：
+ - page lifecycle
+ - onlineState
+ - 利用deviceOrientation制作一个随着手机旋转的正方体
+ - 使用clipboard api完成复制粘贴功能
+ - battery status
+ - custom event
+ - 利用execCommand完成一个简单的富文本
+ 
--- src/components/AmbientLight/index.js
+++ src/components/AmbientLight/index.js
@@ -2,17 +2,40 @@ import React, { Component, Fragment } from "react";
 
 class AmbientLight extends Component {
 
+  sensor = null
+
   state = {
     lux: null
   }
 
-  deviceLightHandler = ev => {
-    this.setState({ lux: ev.value })
-    console.log(`${ev.value} lux`);
+  getLux = () => {
+    this.sensor = new AmbientLightSensor();
+    this.sensor.start();
+    this.sensor.onchange = ev => {
+      this.setState({
+        lux: ev.reading.illuminance
+      })
+    }
   }
 
   componentDidMount() {
-    window.addEventListener('devicelight', this.deviceLightHandler);
+    if (window.AmbientLightSensor) {
+      navigator.permissions.query({ name: 'ambient-light-sensor' }).then(status => {
+        // 代表许可状态
+        if (status.state === 'granted') {
+          this.getLux();
+          // 代表需要授权
+        } else if (status.state === 'promt') {
+
+          // 代表拒绝状态 status.state === 'denied'
+        } else {
+
+        }
+      })
+
+    } else {
+      alert('浏览器不支持~~~~')
+    }
   }
 
   render() {
